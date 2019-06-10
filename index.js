a73e3836cf27d87ed2aacff88e776f1ebc8c2d0a
Apple <apple@Apples-MacBook-Pro-2.local>
2019-06-10 08:44:50 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var cardContainer=$('.card-container');
+var submit=$('.submit-btn');
+submit.on("click",createNewItem);
+function createNewItem(){
+  var item=$('.time').val();
+  appendNewItem(item);
+}
+function appendNewItem(item){
+  cardContainer.append(`<div class="box">
+    <p>${item}</p>
+    <div id="name">
+      <input class="name" type="text">
+      <input class="sub" type="submit">
+    </div>
+    </div>`);
+  $('.time').val("");
+  var name=$('.name');
+  sub.on("click",addName());
+  //var checkbox=$('.checkbox');
+  //checkbox.on("click",remove);
+}
+function addName(){
+  event.target.parentNode.remove();
+}
+
+// function remove(){
+//   event.target.parentNode.remove();
+// }
